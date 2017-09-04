ee04389a366dc8053921b4d6b901dd507f468655
Jessica Mueller <jessie.mueller@gmail.com>
2017-09-03 21:21:25 -0600

    Add classification tree figure to md

--- README.md
+++ README.md
@@ -21,7 +21,19 @@ Several of these approaches to feature engineering have been borrowed from exist
 ## Building a Predictive Model
 
 ### Classification trees
-
+The first attempt at using rpart classification trees, since I was familiar with those methods from some other work.  we defined `train$Survived` as the response variable, then built a tree based on a full feature set of variables of interest.  
+```
+#Define survivability as the response var
+response <- train$Survived
+rpart.tree <- rpart(response ~ Pclass + Sex + Age + SibSp + Parch + Embarked + 
+                    numRooms + cabinDeck + Title + familySize, 
+                    data = train, 
+                    method = "class", 
+                    na.action=na.rpart, 
+                    control = rpart.control(minsplit = 5, cp = 0.008))
+```
+This particular tree's complexity parameter was chosen fairly arbitrarily, and could probably use some tweaking for improved model fit.  Currently this approach yields 77.511% accuracy with the test set.
+![rpart Classification Tree](https://github.com/omgitsjessie/titanic/blob/master/figures/class_tree_full_77511perc.png)
 
 
 ## Build final model
--- titanic.R
+++ titanic.R
@@ -1,7 +1,7 @@
 library(data.table)
 library(ggplot2)
 library(dplyr)
-require(tree) #classification trees
+require(tree) 
 library(rpart)
 library(rattle)
 library(rpart.plot)
@@ -116,28 +116,16 @@ for (i in 1:length(train$Age)) {
     #   }
     # }
 
-summary(train)
-glimpse(train)
 
 
-
-#Build a sample model.
+#TODO - Can you bootstramp sampling to get a better fit model?
 
 #Split train data into train and test, to prelim check model fit.
 trainmodel <- train %>% filter(PassengerId <= 750)
 testmodel <- train %>% filter(PassengerId > 750)
 
-#TODO: Classification Tree WIP
-# survivalModel <- tree(Survived ~ Pclass + Sex + Age + SibSp + Parch + 
-#                       Fare + Embarked + Title + 
-#                       familySize , data = trainmodel)
-# plot(survivalModel)
-# text(survivalModel, cex = .75)
-# summary(survivalModel)
-# survivalPredict <- predict(survivalModel, testmodel)
-
-#Trying WIP rpart
-#define survivability as the response var
+#Classification Tree prediction.
+#Define survivability as the response var
 response <- train$Survived
 #Grow tree
 rpart.tree <- rpart(response ~ Pclass + Sex + Age + SibSp + Parch + Embarked + 
@@ -146,11 +134,8 @@ rpart.tree <- rpart(response ~ Pclass + Sex + Age + SibSp + Parch + Embarked +
                     method = "class", 
                     na.action=na.rpart, 
                     control = rpart.control(minsplit = 5, cp = 0.008))
-fancyRpartPlot(rpart.tree)
-print(rpart.tree)
-#Prune tree
-# pruned.tree <- prune(rpart.tree, cp = 0)
-# fancyRpartPlot(pruned.tree)
+#Plot tree
+plot_fulltree <- fancyRpartPlot(rpart.tree)
 
 #Predict on your set aside test set.
 Prediction <- predict(rpart.tree, testmodel, type = "class")
@@ -164,11 +149,8 @@ mean(submit$Accuracy)
 #Predict on the Real Test Set
 
 ##Clean the Real Test Set.
-#Cabin
-#1. How many rooms, if they're in a cabin?  Regex out spaces and add 1
-
-#This changes every value of numRooms to be the number of spaces + 1 in 
-#the cabin col.  
+#1. How many rooms, if they're in a cabin?  Regex out spaces and add 1.  This 
+#changes every value of numRooms to be the number of ' ' + 1 in the cabin col.  
 test <- transform(test, numRooms = countSpaces(Cabin) + 1)
 
 # What deck were people in?
@@ -189,10 +171,8 @@ test$Title[test$Title %in% rareTitle] <- "Rare Title"
 #2. Grab last name from the name regex 
 test$lastName <- gsub(', .*', '', test$Name)
 
-#Family size
-#New column for family size
+#Family size - create a new column for family size
 test$familySize <- 1 + test$SibSp + test$Parch
-
 
 #Work on missing data points. 86 Age, 327 Cabin/numrooms/cabinDeck, 1 Fare.
 missingData <- sapply(test,function(x) sum(is.na(x)))
@@ -208,12 +188,7 @@ test %>%
   summarise(median = median(Fare, na.rm = T))
 test[c(153), "Fare"] <- 8.05
 
-#2. There are 177 missing Age points.  Predict age from Training Set.
-# ageData <- train %>% filter(!is.na(Age))
-# noAgeData <- train %>% filter(is.na(Age))
-# ageModel <- glm(Age ~ Survived + Pclass + SibSp, 
-#                 data=ageData)
-# summary(ageModel)
+#2. There are 86 missing Age points.  Predict age from Training Age model
 #Apply model only to is.na(Age) passengers
 for (i in 1:length(test$Age)) {
   if (Age = NA) {
@@ -222,26 +197,7 @@ for (i in 1:length(test$Age)) {
 }
 
 
-#3. 687 of missing rooms.. can we find anything there?
-#4. What can we do with Ticket?  
-#5. Any change in survival for families of all adults (all siblings; or
-#non-familial friends sharing a cabin) relative to families with
-#children?
-#5b. What about average family age as that predictor?
-#6. Possible to infer embarkation from nationality of surname?
-#7. Missing Fares - can we build a predictive model for fares, based on
-#embarkation point, class, maybe title?
-#8. Break up title into more categories?
-
-
-#9.  Married but travelling alone? Over 18, sibsp = 0, married
-# train$marriedSolo <- 0
-# for (i in 1:length(train$marriedSolo)) {
-#   if (train[i, "Age"] > 18 && train[i, "SibSp"] == 0 && train[i, "Title"] == "Mrs") {
-#     train[i, "marriedSolo"] <- 1
-#   }
-# }
-
+#There is a new Title in the mix here, so change it to 'Rare Title'
 which(test$Title == "Dona")
 test$Title[415] <- "Rare Title"
 #Predict on Test Data
@@ -249,7 +205,9 @@ PredictionTest <- predict(rpart.tree, test, type = "class")
 #To satisfy submission requirements, these col names must not change below.
 submitTest <- data.frame(PassengerID = test$PassengerId, 
                      Survived = PredictionTest)
-write.csv(submitTest, file = "SubmittedPredictions.csv", row.names = F)
+write.csv(submitTest, 
+    file = "~/R Projects - Data Science/titanic/data/SubmittedPredictions.csv", 
+    row.names = F)
 
 #This iteration scored 77.511% accuracy; not great.  Keep at it.
 
